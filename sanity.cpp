5ccf58a19f0dd9d98a84796cd576027c6dd707f3
Todd Wolfson <todd@twolfson.com>
2018-01-26 17:10:51 -0800

    Added string stream but rewind isn't working

--- sanity.cpp
+++ sanity.cpp
@@ -1,11 +1,21 @@
+// http://www.manticmoo.com/articles/jeff/programming/cpp-stdin.php
+// https://stackoverflow.com/a/17959055
 #include <iostream>
+#include <sstream>
 #include <string>
 
 int main() {
-  // https://stackoverflow.com/a/17959055
+  std::stringstream content;
+  content << std::cin.rdbuf();
   std::string input_line;
-  while(std::cin) {
-    getline(std::cin, input_line);
+
+  while(content) {
+    getline(content, input_line);
+    std::cout << input_line << std::endl;
+  };
+  content.seekg(0);
+  while(content) {
+    getline(content, input_line);
     std::cout << input_line << std::endl;
   };
 
