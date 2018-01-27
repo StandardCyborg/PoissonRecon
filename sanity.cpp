0008eb3787f4712b7b4b02c7276592966fd53597
Todd Wolfson <todd@twolfson.com>
2018-01-26 17:29:44 -0800

    Added more notes for self

--- sanity.cpp
+++ sanity.cpp
@@ -1,5 +1,7 @@
 // http://www.manticmoo.com/articles/jeff/programming/cpp-stdin.php
+// Never mind... stringstream isn't what we want; it's only for double READING IN stdin
 // https://stackoverflow.com/a/17959055
+// http://cpp.indi.frih.net/blog/2014/09/how-to-read-an-entire-file-into-memory-in-cpp/
 #include <iostream>
 #include <sstream>
 #include <string>
@@ -12,12 +14,15 @@ int main() {
 
   // Output our content twice
   std::string line;
+  content.close();
+  std::cout << "rewind" << content.tellp() << std::endl;
   while(content) {
     getline(content, line);
     std::cout << line << std::endl;
   };
-  std::cout << "rewind" << std::endl;
-  content.seekg(100);
+  std::cout << "rewind" << content.tellp() << std::endl;
+  content.seekp(166);
+  std::cout << "rewind" << content.tellp() << std::endl;
   while(content) {
     getline(content, line);
     std::cout << line << std::endl;
