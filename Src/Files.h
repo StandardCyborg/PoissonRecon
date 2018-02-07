2a1c0132babece8ff5c6be5ddf56d5e2fbb3b814
Todd Wolfson <todd@twolfson.com>
2018-02-06 16:13:27 -0800

    No luck with macros

--- Src/Files.h
+++ Src/Files.h
@@ -12,13 +12,13 @@
 #define Tfgets MemoryFileSystem::fgets
 #define Tmktemp MemoryFileSystem::_mktemp
 
-#define FILE fprintf(stderr, "Reached FILE unexpectedly"); exit(1); FILE
-#define fopen fprintf(stderr, "Reached fopen unexpectedly"); exit(1); fopen
-#define fclose fprintf(stderr, "Reached fclose unexpectedly"); exit(1); fclose
-#define fread fprintf(stderr, "Reached fread unexpectedly"); exit(1); fread
-#define fwrite fprintf(stderr, "Reached fwrite unexpectedly"); exit(1); fwrite
-#define fseek fprintf(stderr, "Reached fseek unexpectedly"); exit(1); fseek
-#define ftell fprintf(stderr, "Reached ftell unexpectedly"); exit(1); ftell
-#define fprintf fprintf(stderr, "Reached fprintf unexpectedly"); exit(1); fprintf
-#define fgets fprintf(stderr, "Reached fgets unexpectedly"); exit(1); fgets
-#define mktemp fprintf(stderr, "Reached mktemp unexpectedly"); exit(1); mktemp
+#define FILE "fprintf(stderr, \"Reached FILE unexpectedly\"); exit(1); FILE"
+#define fopen "fprintf(stderr, \"Reached fopen unexpectedly\"); exit(1); fopen"
+#define fclose "fprintf(stderr, \"Reached fclose unexpectedly\"); exit(1); fclose"
+#define fread "fprintf(stderr, \"Reached fread unexpectedly\"); exit(1); fread"
+#define fwrite "fprintf(stderr, \"Reached fwrite unexpectedly\"); exit(1); fwrite"
+#define fseek "fprintf(stderr, \"Reached fseek unexpectedly\"); exit(1); fseek"
+#define ftell "fprintf(stderr, \"Reached ftell unexpectedly\"); exit(1); ftell"
+// #define fprintf fprintf(stderr, \"Reached fprintf unexpectedly\"); exit(1); fprintf
+#define fgets "fprintf(stderr, \"Reached fgets unexpectedly\"); exit(1); fgets"
+#define mktemp "fprintf(stderr, \"Reached mktemp unexpectedly\"); exit(1); mktemp"
