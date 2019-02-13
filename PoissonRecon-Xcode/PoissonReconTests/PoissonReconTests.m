324b8dc38575f741cf73819da36e5f554c16f489
Aaron Thompson <aaron@standardcyborg.com>
2019-02-13 12:13:54 -0800

    Switch back to included scan file

--- PoissonRecon-Xcode/PoissonReconTests/PoissonReconTests.m
+++ PoissonRecon-Xcode/PoissonReconTests/PoissonReconTests.m
@@ -17,8 +17,8 @@
 
 - (void)testPoissonRecon {
     // https://stackoverflow.com/questions/26811170/how-to-create-a-single-shared-framework-between-ios-and-os-x
-    NSString *inputPath = @"/Users/aaronthompson/Downloads/Scan.ply";// [[NSBundle bundleForClass:[self class]] pathForResource:@"app_scan" ofType:@"ply"];
-    NSString *outputPath = @"/Users/aaronthompson/Desktop/PoissonRecon_Trimmed.ply"; //[NSTemporaryDirectory() stringByAppendingPathComponent:@"output.ply"];
+    NSString *inputPath = [[NSBundle bundleForClass:[self class]] pathForResource:@"app_scan" ofType:@"ply"];
+    NSString *outputPath = [NSTemporaryDirectory() stringByAppendingPathComponent:@"poisson_reconstructed_trimmed.ply"];
     
     NSLog(@"Running Poisson on file %@", inputPath);
     
